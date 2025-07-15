0db7d636e17a23eeb6fb8bd451560feb596e701a
Daniel <cd17008@ues.edu.sv>
2025-07-14 20:57:26 -0600

    Cambio de logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,25 +9,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={"https://genero.ues.edu.sv/wp-content/uploads/sites/48/2024/02/cropped-Logo-UES-2-271x300.png"} alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Fondo Circulante FIA-UES</h1>
     </>
   )
 }
